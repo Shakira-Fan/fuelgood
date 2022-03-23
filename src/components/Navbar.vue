c6e2d26202eb2b11e98e56cf416a684fb73be54b
Wen-Lin Lee <82gogolee@gmail.com>
2022-03-23 15:30:37 +0800

    change the style of the navar, and remove cart from the nav

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -1,10 +1,12 @@
 <template>
   <nav class="navbar">
-    <img src="../assets/images/wels.svg" alt="" />
-    <router-link :to="{ name: 'home' }">首頁</router-link>
-    <router-link :to="{ name: 'news' }">優惠活動</router-link>
-    <router-link :to="{ name: 'cart' }">購物車</router-link>
-    <router-link :to="{ name: 'account' }">登入／註冊</router-link>
+    <img src="../assets/images/wels.svg" alt="wels logo" />
+    <div class="navbar-items">
+      <router-link :to="{ name: 'home' }">首頁</router-link>
+      <router-link :to="{ name: 'news' }">優惠活動</router-link>
+
+      <router-link :to="{ name: 'account' }">登入／註冊</router-link>
+    </div>
   </nav>
 </template>
 
@@ -15,16 +17,27 @@ export default {};
 <style scoped>
 .navbar {
   display: flex;
-  justify-content: space-around;
+  justify-content: space-between;
   align-items: center;
   background-color: var(--color-primary);
-  padding: 1.5rem;
+  padding: 2rem;
+}
+.navbar-items {
+  width: 30%;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
 }
 img {
   width: 8rem;
 }
 a {
-  font-size: 2.1rem;
+  font-size: 2.3rem;
   text-decoration: none;
+  color: #000;
+}
+a.router-link-active {
+  border-bottom: 0.3rem solid var(--color-secondary);
+  padding-bottom: 4px;
 }
 </style>
