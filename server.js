81be39e3431f796eae8f3d1f8aa6b332545da9e5
Lucien Peng <kesiang.peng@gmail.com>
2022-03-24 09:11:46 +0800

    fix:cors

--- server.js
+++ server.js
@@ -13,6 +13,22 @@ const swaggerDocument = require("./swagger.json");
 require("./config/passport");
 
 //Middlewares
+// CORS config here
+app.all("/*", function (req, res, next) {
+  // CORS headers
+  res.header("Access-Control-Allow-Origin", "*"); // restrict it to the required domain
+  res.header("Access-Control-Allow-Methods", "GET,PUT,POST,DELETE,OPTIONS");
+  // Set custom headers for CORS
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Content-type,Accept,X-Access-Token,X-Key"
+  );
+  if (req.method == "OPTIONS") {
+    res.status(200).end();
+  } else {
+    next();
+  }
+});
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 app.use(
@@ -43,22 +59,7 @@ mongoose
     console.log(e);
   });
 
-// CORS config here
-app.all("/*", function (req, res, next) {
-  // CORS headers
-  res.header("Access-Control-Allow-Origin", "*"); // restrict it to the required domain
-  res.header("Access-Control-Allow-Methods", "GET,PUT,POST,DELETE,OPTIONS");
-  // Set custom headers for CORS
-  res.header(
-    "Access-Control-Allow-Headers",
-    "Content-type,Accept,X-Access-Token,X-Key"
-  );
-  if (req.method == "OPTIONS") {
-    res.status(200).end();
-  } else {
-    next();
-  }
-});
+
 
 app.get("/", (req, res) => {
   res.render("index.ejs", { user: req.user });
