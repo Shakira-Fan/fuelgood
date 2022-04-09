e382597493ad759298aea49a1b4fe01e53ede088
Wen-Lin Lee <82gogolee@gmail.com>
2022-04-09 19:52:49 +0800

    change the date format to add @click to buttons

--- src/views/UserView.vue
+++ src/views/UserView.vue
@@ -1,84 +1,158 @@
 <template>
-<div class="user-page container">
-    <div class="loading animate__animated animate__bounce animate__infinite" v-if="loading">載入中...</div>
+  <div class="user-page container">
+    <div
+      class="loading animate__animated animate__bounce animate__infinite"
+      v-if="loading"
+    >
+      載入中...
+    </div>
 
     <div>
-        <ul v-if="!loading" id="navbar-example2">
-          <li class="nav-item">
-            <a class="nav-link" @click="handleOrder" href="#scrollspyHeading1">歷史訂單</a>
-          </li>
-          <li class="nav-item">
-            <a data-bs-toggle="modal" data-bs-target="#staticBackdrop" class="nav-link" @click="handleQr" href="#scrollspyHeading2">領用汽油</a>
+      <ul v-if="!loading" id="navbar-example2">
+        <li class="nav-item">
+          <a class="nav-link" @click="handleOrder" href="#order">歷史訂單</a>
         </li>
         <li class="nav-item">
-            <a class="nav-link" @click="handleQr" href="#scrollspyHeading2">添購汽油</a>
+          <a
+            data-bs-toggle="modal"
+            data-bs-target="#staticBackdrop"
+            class="nav-link"
+            @click="handleQr"
+            href="#scrollspyHeading2"
+            >領用汽油</a
+          >
         </li>
-        </ul>
-    </div>  
+        <li class="nav-item">
+          <a
+            class="nav-link"
+            @click="$router.push('/cart')"
+            href="#scrollspyHeading2"
+            >添購汽油</a
+          >
+        </li>
+      </ul>
+    </div>
 
     <div class="row flex justify-content-center align-items-center">
-
       <div class="profile">
-          <i class="bi bi-person-circle" v-if="!loading"></i>
-          <p class="mt-3" v-if="!loading"><strong>{{ username }}</strong>，歡迎回來！</p>
+        <i class="bi bi-person-circle" v-if="!loading"></i>
+        <p class="mt-3" v-if="!loading">
+          <strong>{{ username }}</strong
+          >，歡迎回來！
+        </p>
       </div>
 
-      <div class="row m-5 flex align-items-center justify-content-center inventory-container" v-if="!loading">
-          <h2 class="mb-5" v-if="!loading">未提取庫存</h2>
-          <div class="row flex-lg-nowrap flex-wrap flex justify-content-center">
-              <div class="inventory col-lg-3 col-10 col-md-5 flex-sm-fill py-5 flex-shrink-2">
-                  <i class="bi animate__animated animate__pulse animate__infinite bi-droplet mb-3"></i>
-                  <!-- 92無鉛汽油 -->
-                  <h2 class="gas-title">無鉛汽油</h2>
-                  <h3 class="actual-inventory">
-                      {{ inventory[0].properties['92無鉛汽油'].liter }}
-                  </h3>
-                  <p class="unit">公升</p>
-                  <button class="btn btn-deduct">領用−</button>
-                  <button class="btn btn-add">添購＋</button>
-              </div>
-
-              <div class="inventory col-lg-3 col-10 col-md-5 flex-fill py-5 flex-shrink-2">
-                  <i class="bi animate__animated animate__pulse animate__infinite bi-droplet mb-3"></i>
-                  <!-- 95無鉛汽油 -->
-                  <h2 class="gas-title">無鉛汽油:</h2>
-                  <h3 class="actual-inventory">
-                      {{ inventory[0].properties['95無鉛汽油'].liter }}
-                  </h3>
-                  <p class="unit">公升</p>
-                  <button class="btn btn-deduct">領用−</button>
-                  <button class="btn btn-add">添購＋</button>
-              </div>
-
-              <div class="inventory col-lg-3 col-10 col-md-5 flex-fill py-5 flex-shrink-2">
-                  <i class="bi animate__animated animate__pulse animate__infinite bi-droplet mb-3"></i>
-                  <!-- 98無鉛汽油 -->
-                  <h2 class="gas-title">無鉛汽油</h2>
-                  <h3 class="actual-inventory">
-                      {{ inventory[0].properties['98無鉛汽油'].liter }}
-                  </h3>
-                  <p class="unit">公升</p>
-                  <button class="btn btn-deduct">領用−</button>
-                  <button class="btn btn-add">添購＋</button>
-              </div>
-
-              <div class="inventory col-lg-3 col-10 col-md-5 flex-fill py-5 flex-shrink-2">
-                  <i class="bi animate__animated animate__pulse animate__infinite bi-droplet mb-3"></i>
-                  <!-- 超級柴油 -->
-                  <h2 class="gas-title">超級柴油</h2>
-                  <h3 class="actual-inventory">
-                      {{ inventory[0].properties['高級柴油'].liter }}
-                  </h3>
-                  <p class="unit">公升</p>
-                  <button class="btn btn-deduct">領用−</button>
-                  <button class="btn btn-add">添購＋</button>
-              </div>
+      <div
+        class="row m-5 flex align-items-center justify-content-center inventory-container"
+        v-if="!loading"
+      >
+        <h2 class="mb-5" v-if="!loading">未提取庫存</h2>
+        <div class="row flex-lg-nowrap flex-wrap flex justify-content-center">
+          <div
+            class="inventory col-lg-3 col-10 col-md-5 flex-sm-fill py-5 flex-shrink-2"
+          >
+            <i
+              class="bi animate__animated animate__pulse animate__infinite bi-droplet mb-3"
+            ></i>
+            <!-- 92無鉛汽油 -->
+            <h2 class="gas-title">無鉛汽油</h2>
+            <h3 class="actual-inventory">
+              {{ inventory[0].properties['92無鉛汽油'].liter }}
+            </h3>
+            <p class="unit">公升</p>
+            <button
+              class="btn btn-deduct"
+              @click="handleQr"
+              data-bs-toggle="modal"
+              data-bs-target="#staticBackdrop"
+            >
+              領用−
+            </button>
+            <button class="btn btn-add" @click="$router.push('/cart')">
+              添購＋
+            </button>
           </div>
+
+          <div
+            class="inventory col-lg-3 col-10 col-md-5 flex-fill py-5 flex-shrink-2"
+          >
+            <i
+              class="bi animate__animated animate__pulse animate__infinite bi-droplet mb-3"
+            ></i>
+            <!-- 95無鉛汽油 -->
+            <h2 class="gas-title">無鉛汽油:</h2>
+            <h3 class="actual-inventory">
+              {{ inventory[0].properties['95無鉛汽油'].liter }}
+            </h3>
+            <p class="unit">公升</p>
+            <button
+              class="btn btn-deduct"
+              @click="handleQr"
+              data-bs-toggle="modal"
+              data-bs-target="#staticBackdrop"
+            >
+              領用−
+            </button>
+            <button class="btn btn-add" @click="$router.push('/cart')">
+              添購＋
+            </button>
+          </div>
+
+          <div
+            class="inventory col-lg-3 col-10 col-md-5 flex-fill py-5 flex-shrink-2"
+          >
+            <i
+              class="bi animate__animated animate__pulse animate__infinite bi-droplet mb-3"
+            ></i>
+            <!-- 98無鉛汽油 -->
+            <h2 class="gas-title">無鉛汽油</h2>
+            <h3 class="actual-inventory">
+              {{ inventory[0].properties['98無鉛汽油'].liter }}
+            </h3>
+            <p class="unit">公升</p>
+            <button
+              class="btn btn-deduct"
+              @click="handleQr"
+              data-bs-toggle="modal"
+              data-bs-target="#staticBackdrop"
+            >
+              領用−
+            </button>
+            <button class="btn btn-add" @click="$router.push('/cart')">
+              添購＋
+            </button>
+          </div>
+
+          <div
+            class="inventory col-lg-3 col-10 col-md-5 flex-fill py-5 flex-shrink-2"
+          >
+            <i
+              class="bi animate__animated animate__pulse animate__infinite bi-droplet mb-3"
+            ></i>
+            <!-- 超級柴油 -->
+            <h2 class="gas-title">超級柴油</h2>
+            <h3 class="actual-inventory">
+              {{ inventory[0].properties['高級柴油'].liter }}
+            </h3>
+            <p class="unit">公升</p>
+            <button
+              class="btn btn-deduct"
+              @click="handleQr"
+              data-bs-toggle="modal"
+              data-bs-target="#staticBackdrop"
+            >
+              領用−
+            </button>
+            <button class="btn btn-add" @click="$router.push('/cart')">
+              添購＋
+            </button>
+          </div>
+        </div>
       </div>
 
       <div class="container order-table p-3 m-5" v-if="listOrder">
         <div class="row">
-          <h2 class="m-5" v-if="listOrder">歷史訂單</h2>
+          <h2 class="m-5" v-if="listOrder" id="order">歷史訂單</h2>
           <div class="col">
             <table class="table table-hover table-striped">
               <thead>
@@ -105,29 +179,50 @@
           </div>
         </div>
       </div>
-
     </div>
 
     <!-- Modal -->
-    <div class="modal fade" id="staticBackdrop" data-bs-backdrop="static" data-bs-keyboard="false" tabindex="-1" aria-labelledby="staticBackdropLabel" aria-hidden="true">
+    <div
+      class="modal fade"
+      id="staticBackdrop"
+      data-bs-backdrop="static"
+      data-bs-keyboard="false"
+      tabindex="-1"
+      aria-labelledby="staticBackdropLabel"
+      aria-hidden="true"
+    >
       <div class="modal-dialog modal-lg">
         <div class="modal-content">
           <div class="modal-header">
             <h2 class="modal-title" id="staticBackdropLabel">領用汽油</h2>
-            <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+            <button
+              type="button"
+              class="btn-close"
+              data-bs-dismiss="modal"
+              aria-label="Close"
+            ></button>
           </div>
           <div class="modal-body">
-            <img class="img-fluid" src="https://images.pexels.com/photos/278430/pexels-photo-278430.jpeg?cs=srgb&dl=pexels-pixabay-278430.jpg&fm=jpg" alt="">
+            <img
+              class="img-fluid"
+              src="https://images.pexels.com/photos/278430/pexels-photo-278430.jpeg?cs=srgb&dl=pexels-pixabay-278430.jpg&fm=jpg"
+              alt=""
+            />
           </div>
           <div class="modal-footer">
-            <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+            <button
+              type="button"
+              class="btn btn-secondary"
+              data-bs-dismiss="modal"
+            >
+              Close
+            </button>
             <button type="button" class="btn btn-primary">完成</button>
           </div>
         </div>
       </div>
     </div>
-
-</div>
+  </div>
 </template>
 
 <script>
@@ -164,34 +259,42 @@ export default {
   },
   methods: {
     localDate(utcDate) {
-      return moment.utc(utcDate).local().format('YYYY-MM-DD,h:mm:ss a');
+      return moment.utc(utcDate).local().format('YYYY/MM/DD,HH:mm');
     },
     handleQr() {
       this.qr = !this.qr;
     },
     handleOrder() {
-      this.listOrder = !this.listOrder;
+      this.listOrder = true;
     },
   },
   async created() {
     this.loading = true;
-    const res = await axios.get('https://fuel-good.herokuapp.com/order/all');
-    console.log(res);
-    if (!this.orders.length) {
-      await this.orders.push(
-        res.data.filter(order => {
-          return order.email === localStorage.getItem('email');
-        })
-      );
+
+    try {
+      const res = await axios.get('https://fuel-good.herokuapp.com/order/all');
+      console.log(res);
+      if (!this.orders.length) {
+        await this.orders.push(
+          res.data.filter(order => {
+            return order.email === localStorage.getItem('email');
+          })
+        );
+      }
+    } catch (err) {
+      console.log(err.message);
     }
 
-    const res2 = await axios.get(
-      `https://fuel-good.herokuapp.com/admin/user/${this.email}`
-    );
-    console.log(res2);
-
-    if (!this.inventory.length) {
-      this.inventory.push(res2.data);
+    try {
+      const res2 = await axios.get(
+        `https://fuel-good.herokuapp.com/admin/user/${this.email}`
+      );
+      console.log(res2);
+      if (!this.inventory.length) {
+        this.inventory.push(res2.data);
+      }
+    } catch (err) {
+      console.log(err.message);
     }
     this.loading = false;
   },
@@ -200,94 +303,90 @@ export default {
 
 <style lang="scss" scoped>
 @import 'bootstrap/scss/bootstrap';
-@import url("https://cdn.jsdelivr.net/npm/bootstrap-icons@1.3.0/font/bootstrap-icons.css");
+@import url('https://cdn.jsdelivr.net/npm/bootstrap-icons@1.3.0/font/bootstrap-icons.css');
 
 .user-page {
-    min-height: 100vh;
-    margin-top: 5rem;
+  min-height: 100vh;
+  margin-top: 5rem;
 
-    ul{
-      list-style-type: none;
-      display: flex;
-      justify-content: end;
-      li a{
-        color: #0e3365;
-        &:hover{
-           color: var(--color-primary)
-        }
+  ul {
+    list-style-type: none;
+    display: flex;
+    justify-content: end;
+    li a {
+      color: #0e3365;
+      &:hover {
+        color: var(--color-primary);
       }
-
     }
+  }
 
-    .inventory {
+  .inventory {
     margin: 1rem;
     border-radius: 2rem;
     box-shadow: 0px 5px 30px -5px rgba(0, 0, 0, 0.17);
 
     button {
-    padding: 0 0.5rem;
-    margin: 0.5rem;
-    border: none;
-    color: white;
-    font-size:1.5rem ;
-    border-radius: 1.5rem;
+      padding: 0 0.5rem;
+      margin: 0.5rem;
+      border: none;
+      color: white;
+      font-size: 1.5rem;
+      border-radius: 1.5rem;
 
-    &.btn-add{
+      &.btn-add {
         background-color: var(--color-button);
         &:hover {
-        background-color: var(--color-button-hover);
+          background-color: var(--color-button-hover);
         }
-    }
-      &.btn-deduct{
+      }
+      &.btn-deduct {
         background-color: #f28482;
         &:hover {
-        background-color: #f28482c5;
+          background-color: #f28482c5;
         }
-    }
-  }
-
-
-  &:hover{
-    transition: all ease-out 0.3s;
-    box-shadow:
-    0px 0px 2.7px rgba(0, 0, 0, 0.042),
-    0px 0px 7.5px rgba(0, 0, 0, 0.06),
-    0px 0px 18.1px rgba(0, 0, 0, 0.078),
-    0px 0px 60px rgba(0, 0, 0, 0.12)
-    }
-
-  i{
-    font-size: 10rem;
-    position: relative;
-    display: block;
-    color: #0e3365;
-    &::after{
-    font-size: 4rem;
-    position: absolute;
-    top: 50%;
-    right: 0;
-    width: 100%;
-    height: 100% ;
-    }
-  }
-    &:nth-child(1){
-      i::after{
-        content:"92";
       }
     }
-    &:nth-child(2){
-      i::after{
-        content:"95";
+
+    &:hover {
+      transition: all ease-out 0.3s;
+      box-shadow: 0px 0px 2.7px rgba(0, 0, 0, 0.042),
+        0px 0px 7.5px rgba(0, 0, 0, 0.06), 0px 0px 18.1px rgba(0, 0, 0, 0.078),
+        0px 0px 60px rgba(0, 0, 0, 0.12);
+    }
+
+    i {
+      font-size: 10rem;
+      position: relative;
+      display: block;
+      color: #0e3365;
+      &::after {
+        font-size: 4rem;
+        position: absolute;
+        top: 50%;
+        right: 0;
+        width: 100%;
+        height: 100%;
       }
     }
-    &:nth-child(3){
-      i::after{
-        content:"98";
+    &:nth-child(1) {
+      i::after {
+        content: '92';
       }
     }
-    &:nth-child(4){
-      i::after{
-        content:"柴";
+    &:nth-child(2) {
+      i::after {
+        content: '95';
+      }
+    }
+    &:nth-child(3) {
+      i::after {
+        content: '98';
+      }
+    }
+    &:nth-child(4) {
+      i::after {
+        content: '柴';
       }
     }
   }
@@ -297,17 +396,15 @@ export default {
     color: #0e3365;
   }
 
-  .modal.fade{
-
-  border: none;
-  border-radius: 1.5rem;
-}
-
-}
-
-  h2 {
-    font-size: 4rem;
+  .modal.fade {
+    border: none;
+    border-radius: 1.5rem;
   }
+}
+
+h2 {
+  font-size: 4rem;
+}
 
 .unit {
   font-size: 2.3rem;
@@ -329,9 +426,6 @@ export default {
   box-shadow: 1px 2px 5px rgba(0, 0, 0, 0.1);
 }
 
-
-
-
 .order-info {
   margin-right: 0.5rem;
 }
@@ -343,5 +437,4 @@ export default {
   padding: 30rem;
   font-size: 3rem;
 }
-
 </style>
