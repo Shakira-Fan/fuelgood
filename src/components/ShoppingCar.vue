33e8b83afbc015660432d972b6ae1a96025f5252
Wen-Lin Lee <82gogolee@gmail.com>
2022-04-12 21:43:59 +0800

    modify the style of the cart component

--- src/components/ShoppingCar.vue
+++ src/components/ShoppingCar.vue
@@ -1,31 +1,53 @@
 <template>
   <div class="col-10 col-sm-6 com-md-5 col-lg-3">
     <div class="card h-100">
-        <div class="card-title pt-5">
-            <i class="bi bi-droplet-half"></i>
-            {{ product.gasoline }}
-        </div>
-        <div class="card-body">
-            <form>
-                <h3 class="py-3 ">${{ product.price }}</h3>
-                <span><input class="qty-input py-2" type="number" v-model.number="quantity" /> 公升</span>
-            </form>
-            <p class="my-3 animate__animated animate__pulse animate__infinite animate__slow total">50</p>
-            <button @click="addToCart(product.gasoline, quantity)" class="btn btn-add mb-3">
-                加入油桶
-            </button>
-            <button @click="addToCart(product.gasoline, quantity)" class="btn btn-deduct mb-3">
-                清除全部
-            </button>
-        </div>        
+      <div class="card-title pt-5">
+        <i class="bi bi-droplet-half"></i>
+        {{ product.gasoline }}
+      </div>
+      <div class="card-body">
+        <form>
+          <h3 class="py-3">{{ product.price }}元</h3>
+          <span
+            ><input
+              class="qty-input py-2"
+              type="number"
+              v-model.number="quantity"
+            />
+            公升</span
+          >
+        </form>
+        <!-- <p
+          class="my-3 animate__animated animate__pulse animate__infinite animate__slow total show-total"
+        >
+          {{ (quantity = qty) }}
+        </p> -->
+        <button
+          @click="addToCart(product.gasoline, quantity)"
+          class="btn btn-add mb-3 mt-4"
+        >
+          加入油桶
+        </button>
+        <!-- <button
+          @click="addToCart(product.gasoline, quantity)"
+          class="btn btn-deduct mb-3"
+        >
+          清除全部
+        </button> -->
+      </div>
     </div>
   </div>
 </template>
 
 <script>
-import "animate.css";
+import 'animate.css';
 export default {
-  props: ["product", "index", "addToCart"],
+  data() {
+    return {
+      qty: null,
+    };
+  },
+  props: ['product', 'index', 'addToCart'],
   created() {
     localStorage[this.product.gasoline] = 0;
   },
@@ -34,15 +56,18 @@ export default {
       return localStorage[this.product.gasoline];
     },
   },
+  computed: {
+    total() {
+      this.product.price * this.quantity;
+    },
+  },
 };
 </script>
 
 <style lang="scss" scoped>
 @import 'bootstrap/scss/bootstrap';
 
-
 .card {
-  
   border-radius: 1.5rem;
   box-shadow: 0px 5px 30px -5px rgba(0, 0, 0, 0.17);
   border: none;
@@ -50,62 +75,57 @@ export default {
     font-size: 3rem;
   }
   .qty-input {
-  border: 2px solid white;
-  padding: 0.2rem;
-  border-radius: 1rem;
-  box-shadow: 0px 5px 30px -5px rgba(0, 0, 0, 0.17);
-  width: 30%;
-  &:hover{
+    border: 2px solid white;
+    padding: 0.2rem;
+    border-radius: 1rem;
+    box-shadow: 0px 5px 30px -5px rgba(0, 0, 0, 0.17);
+    width: 30%;
+    &:hover {
       border: 2px solid var(--color-primary);
     }
   }
 
-  h3{
+  h3 {
     font-size: 5rem;
     color: red;
   }
 
-  &:hover{
-  transition: all ease-out 0.3s;
-  box-shadow:
-  0px 0px 2.7px rgba(0, 0, 0, 0.042),
-  0px 0px 7.5px rgba(0, 0, 0, 0.06),
-  0px 0px 18.1px rgba(0, 0, 0, 0.078),
-  0px 0px 60px rgba(0, 0, 0, 0.12)
+  &:hover {
+    transition: all ease-out 0.3s;
+    box-shadow: 0px 0px 2.7px rgba(0, 0, 0, 0.042),
+      0px 0px 7.5px rgba(0, 0, 0, 0.06), 0px 0px 18.1px rgba(0, 0, 0, 0.078),
+      0px 0px 60px rgba(0, 0, 0, 0.12);
   }
-
-
 }
-  .total{
-    font-size: 3rem;
+.total {
+  font-size: 3rem;
 
-    &::after{
-    content:"TWD";
+  &::after {
+    content: 'TWD';
+  }
+}
+
+button {
+  padding: 0 0.5rem;
+  margin: 0.5rem;
+  border: none;
+  color: white !important;
+  font-size: 1.5rem !important;
+  border-radius: 1.5rem !important;
+
+  &.btn-add {
+    background-color: var(--color-button);
+    &:hover {
+      background-color: var(--color-button-hover);
     }
   }
-
-  button {
-    padding: 0 0.5rem;
-    margin: 0.5rem;
-    border: none;
-    color: white !important;
-    font-size: 1.5rem !important;
-    border-radius: 1.5rem !important;
-
-    &.btn-add {
-      background-color: var(--color-button);
-      &:hover {
-        background-color: var(--color-button-hover);
-      }
-    }
-    &.btn-deduct {
-      background-color: #f28482;
-      &:hover {
-        background-color: #f28482c5;
-      }
+  &.btn-deduct {
+    background-color: #f28482;
+    &:hover {
+      background-color: #f28482c5;
     }
   }
-
+}
 
 .swal2-styled.swal2-confirm {
   background-color: var(--color-secondary);
--- src/components/SidebarComponent.vue
+++ src/components/SidebarComponent.vue
@@ -1,60 +1,45 @@
 <template>
-  <aside class="cart-container">
-    <div class="cart">
-      <h1 class="cart-title spread">
-        <span>
-          購買清單
-          <i class="icofont-cart-alt icofont-1x"></i>
-        </span>
-        <button @click="toggle" class="cart-close">&times;</button>
-      </h1>
+  <div class="cart-container" v-if="Object.keys(cart).length">
+    <table class="table">
+      <thead>
+        <tr>
+          <th scope="col">產品</th>
+          <th scope="col">價格</th>
+          <th scope="col">數量</th>
+          <th scope="col">小計</th>
+          <th><span class="sr-only">刪除</span></th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr v-for="(quantity, key, i) in cart" :key="i">
+          <th scope="row">{{ key }}</th>
+          <td>{{ getPrice(key) }}</td>
+          <td>{{ quantity }}</td>
+          <td>{{ (quantity * getPrice(key)).toFixed(2) }}</td>
+          <td>
+            <button @click="remove(key)" class="btn btn-light cart-remove">
+              &times;
+            </button>
+          </td>
+        </tr>
+      </tbody>
+    </table>
 
-      <div class="cart-body">
-        <table class="cart-table">
-          <thead>
-            <tr>
-              <th>產品</th>
-              <th>價格</th>
-              <th>數量</th>
-              <th>小計</th>
-              <th><span class="sr-only">刪除</span></th>
-            </tr>
-          </thead>
-          <tbody>
-            <tr v-for="(quantity, key, i) in cart" :key="i">
-              <td>{{ key }}</td>
-              <td>${{ getPrice(key) }}</td>
-              <td class="center">{{ quantity }}</td>
-              <td>${{ (quantity * getPrice(key)).toFixed(2) }}</td>
-              <td class="center">
-                <button @click="remove(key)" class="btn btn-light cart-remove">
-                  &times;
-                </button>
-              </td>
-            </tr>
-          </tbody>
-        </table>
-
-        <p class="center" v-if="!Object.keys(cart).length">
-          <em>購物車內沒有商品～～～</em>
-        </p>
-        <div class="spread">
-          <span><strong>總計:</strong>${{ calculateTotal() }}</span>
-          <router-link :to="'/create-order'" :calculateTotal="calculateTotal"
-            ><button class="btn btn-light">結帳</button></router-link
-          >
-        </div>
-      </div>
+    <div class="spread">
+      <span>共計 {{ calculateTotal() }} 元整</span>
+      <router-link :to="'/create-order'" :calculateTotal="calculateTotal">
+        <button class="confirm-btn">確定訂單</button>
+      </router-link>
     </div>
-  </aside>
+  </div>
 </template>
 
 <script>
 export default {
-  props: ["toggle", "cart", "inventory", "remove"],
+  props: ['toggle', 'cart', 'inventory', 'remove'],
   methods: {
     getPrice(gasoline) {
-      const product = this.inventory.find((p) => {
+      const product = this.inventory.find(p => {
         return p.gasoline === gasoline;
       });
       return product.price;
@@ -71,77 +56,29 @@ export default {
 };
 </script>
 
-<style scoped>
-.cart-container,
-.cart {
-  top: 55px;
-  right: 0px;
-  width: 410px;
-  height: 70%;
-}
-.cart-container {
-  position: fixed;
-  overflow: hidden;
-}
-.cart {
-  position: relative;
-  background-color: var(--color-grey);
-  opacity: 95%;
-  color: black;
-  transition: 1s ease right;
-  border-radius: 5px;
-  border-radius: 2rem;
-}
+<style lang="scss" scoped>
+@import 'bootstrap/scss/bootstrap';
 
-.cart-title {
-  align-items: center;
-  min-height: 49px;
-  max-height: 49px;
-  background: #2a599c;
-  border-bottom: 1px solid #fff8ea;
-  padding: 0px 15px;
-  font-size: 26px;
-  color: white;
-  border-radius: 1.3rem 1.3rem 0 0;
-}
-.cart-table {
-  width: 398px;
-  border-collapse: collapse;
-  margin: 5px;
-  font-size: 18px;
-  justify-content: space-between;
-  line-height: 3;
-}
 .spread {
   display: flex;
   justify-content: space-between;
+  flex-direction: column;
   padding: 0 10px 0 10px;
   margin-top: 2rem;
 }
-.cart-close {
-  background: transparent;
-  border: 0px;
-  border-radius: 3px;
-  padding: 0px 13px;
-  color: #fcf5d8;
-  font-size: 32px;
-  font-weight: bold;
-  cursor: pointer;
-}
-.cart-close:hover {
-  background: rgb(28, 28, 46);
-}
-button {
-  border: 0px;
-  border-radius: 1rem;
-  padding: 5px 12px;
-  font-weight: bold;
-  vertical-align: middle;
-  font-size: 1.8rem;
-  cursor: pointer;
+.cart-remove {
   background-color: var(--color-primary);
+  border-radius: 1rem;
+  font-size: 2rem;
+  line-height: 2rem;
+  text-align: center;
+  padding: 0.5rem 1rem;
 }
-button:hover {
-  background-color: #fac609;
+.confirm-btn {
+  background-color: var(--color-primary);
+  padding: 0.5rem;
+  border: none;
+  border-radius: 1rem;
+  margin-top: 2rem;
 }
 </style>
